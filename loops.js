1ee871173313d280c3d4ab3182707a196647756a
rolandzagonyi@gmail.com <rolandzagonyi@gmail.com>
2018-08-03 22:13:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
 for (var i = 1; i <= 25; i++) {
- array.push('I am ${i} strange loop') 
+ array.push('I am ${i} strange loop${i === 0 ? '' : 's'}."') 
  return array
 }
 }
